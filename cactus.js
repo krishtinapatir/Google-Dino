9193116a3d9474ad5251fb6416d5f5525614e1d4
krishtinapatir <122298620+krishtinapatir@users.noreply.github.com>
2023-12-28 00:27:41 +0530

    Create cactus.js

---
+++ cactus.js
@@ -0,0 +1,18 @@
+import {
+  setCustomProperty,
+  incrementCustomProperty,
+  getCustomProperty,
+} from "./updateCustomProperty.js"
+
+const SPEED = 0.05
+const CACTUS_INTERVAL_MIN = 500
+const CACTUS_INTERVAL_MAX = 2000
+const worldElem = document.querySelector("[data-world]")
+
+let nextCactusTime
+export function setupCactus() {
+  nextCactusTime = CACTUS_INTERVAL_MIN
+  document.querySelectorAll("[data-cactus]").forEach(cactus => {
+    cactus.remove()
+  })
+}
